9dc69c377444f213fe6957b0303e538c8c39dec8
Josh Medeiros <josh@jamedeus.com>
2024-01-26 22:57:53 -0800

    Add django app + basic models
    Plant is instantiated when new QR code scanned (takes UUID as id arg)

---
+++ backend/plant_tracker/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+class Plant(models.Model):
+    # UUID of QR code attached to plant
+    id = models.UUIDField(primary_key=True, editable=True)
+
+    # Description fields are optional, if blank user will just see "Unnamed plant"
+    name = models.CharField(max_length=50, blank=True, null=True)
+    species = models.CharField(max_length=50, blank=True, null=True)
+    description = models.CharField(max_length=500, blank=True, null=True)
+
+    # Accept pot sizes between 1 inch and 3 feet (optional)
+    pot_size = models.PositiveIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(36)],
+        blank=True,
+        null=True
+    )
+
+
+class WaterEvent(models.Model):
+    plant = models.ForeignKey(Plant, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+class FertilizeEvent(models.Model):
+    plant = models.ForeignKey(Plant, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
