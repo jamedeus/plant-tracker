75debbe7d8e435753fcabe22dfbfd40befedfe91
Josh Medeiros <josh@jamedeus.com>
2024-02-07 16:51:08 -0800

    Linting: Add missing docstrings, reorder

--- backend/plant_tracker/views.py
+++ backend/plant_tracker/views.py
@@ -8,8 +8,8 @@ from django.shortcuts import render
 from django.core.exceptions import ValidationError
 from django.http import JsonResponse, HttpResponseRedirect
 
-from .models import Tray, Plant, WaterEvent, FertilizeEvent
 from generate_qr_code_grid import generate_layout
+from .models import Tray, Plant, WaterEvent, FertilizeEvent
 
 
 def get_plant_by_uuid(uuid):
@@ -120,15 +120,10 @@ def get_timestamp_from_post_body(func):
     return wrapper
 
 
-def overview(request):
-    context = {
-        'plants': Plant.objects.all(),
-        'trays': Tray.objects.all()
-    }
-    return render(request, 'plant_tracker/overview.html', context)
-
-
 def get_qr_codes(request):
+    '''Returns printer-sized grid of QR code links as base64-encoded PNG
+    QR codes point to manage endpoint, can be used for plants or trays
+    '''
     qr_codes = generate_layout()
     image = BytesIO()
     qr_codes.save(image, format="PNG")
@@ -136,8 +131,46 @@ def get_qr_codes(request):
     return JsonResponse({'qr_codes': image_base64})
 
 
+def overview(request):
+    '''Renders the overview page (shows existing plants/trays, or setup if none)
+    '''
+    context = {
+        'plants': Plant.objects.all(),
+        'trays': Tray.objects.all()
+    }
+    return render(request, 'plant_tracker/overview.html', context)
+
+
+def manage(request, uuid):
+    '''Renders plant/tray management pages, or registration page if UUID is new
+    Accessed by scanning QR code sticker
+    '''
+    # Look up UUID in plant database, render template if found
+    plant = get_plant_by_uuid(uuid)
+    if plant:
+        context = {'plant': plant, 'trays': Tray.objects.all()}
+        return render(request, 'plant_tracker/manage_plant.html', context)
+
+    # Loop up UUID in tray database, render template if found
+    tray = get_tray_by_uuid(uuid)
+    if tray:
+        context = {
+            'tray': tray,
+            'details': tray.get_plant_details(),
+            'options': get_plant_options()
+        }
+        return render(request, 'plant_tracker/manage_tray.html', context)
+
+    # Redirect to registration form if UUID does not exist in either database
+    return render(request, 'plant_tracker/register.html', {'new_id': uuid})
+
+
 @requires_json_post
 def register(data):
+    '''Creates a Plant or Tray database entry with params from POST body
+    Called from form on manage page shown after scanning a new QR code
+    '''
+
     # Replace empty strings with None (prevent empty strings in db)
     data = {key: (value if value != '' else None) for key, value in data.items()}
     print(json.dumps(data, indent=4))
@@ -164,6 +197,9 @@ def register(data):
 @requires_json_post
 @get_plant_from_post_body
 def edit_plant_details(plant, data):
+    '''Updates existing Plant database entry with params from POST body
+    Called from form in edit plant modal on manage page
+    '''
     print(json.dumps(data, indent=4))
 
     # Replace empty strings with None (prevent empty strings in db)
@@ -183,6 +219,9 @@ def edit_plant_details(plant, data):
 @requires_json_post
 @get_tray_from_post_body
 def edit_tray_details(tray, data):
+    '''Updates existing Tray database entry with params from POST body
+    Called from form in edit tray modal on manage page
+    '''
     print(json.dumps(data, indent=4))
 
     # Replace empty strings with None (prevent empty strings in db)
@@ -197,31 +236,11 @@ def edit_tray_details(tray, data):
     return HttpResponseRedirect(f'/manage/{data["tray_id"]}')
 
 
-def manage(request, uuid):
-    # Look up UUID in plant database, render template if found
-    plant = get_plant_by_uuid(uuid)
-    if plant:
-        context = {'plant': plant, 'trays': Tray.objects.all()}
-        return render(request, 'plant_tracker/manage_plant.html', context)
-
-    # Loop up UUID in tray database, render template if found
-    tray = get_tray_by_uuid(uuid)
-    if tray:
-        context = {
-            'tray': tray,
-            'details': tray.get_plant_details(),
-            'options': get_plant_options()
-        }
-        return render(request, 'plant_tracker/manage_tray.html', context)
-
-    # Redirect to registration form if UUID does not exist in either database
-    return render(request, 'plant_tracker/register.html', {'new_id': uuid})
-
-
 @requires_json_post
 @get_plant_from_post_body
 def delete_plant(plant, data):
-    # Delete plant, reload overview page
+    '''Deletes an existing Plant from database, returns redirect to overview
+    '''
     plant.delete()
     return HttpResponseRedirect('/')
 
@@ -229,7 +248,8 @@ def delete_plant(plant, data):
 @requires_json_post
 @get_tray_from_post_body
 def delete_tray(tray, data):
-    # Delete tray, reload overview page
+    '''Deletes an existing Tray from database, returns redirect to overview
+    '''
     tray.delete()
     return HttpResponseRedirect('/')
 
@@ -238,7 +258,9 @@ def delete_tray(tray, data):
 @get_plant_from_post_body
 @get_timestamp_from_post_body
 def water_plant(plant, timestamp, data):
-    # Create new water event, add override timestamp if arg passed
+    '''Creates new WaterEvent for specified Plant entry
+    Requires POST with JSON body containing plant_id and timestamp keys
+    '''
     WaterEvent.objects.create(
         plant=plant,
         timestamp=timestamp
@@ -250,7 +272,9 @@ def water_plant(plant, timestamp, data):
 @get_plant_from_post_body
 @get_timestamp_from_post_body
 def fertilize_plant(plant, timestamp, data):
-    # Create new water event, add override timestamp if arg passed
+    '''Creates new FertilizeEvent for specified Plant entry
+    Requires POST with JSON body containing plant_id and timestamp keys
+    '''
     FertilizeEvent.objects.create(
         plant=plant,
         timestamp=timestamp
@@ -262,6 +286,9 @@ def fertilize_plant(plant, timestamp, data):
 @get_tray_from_post_body
 @get_timestamp_from_post_body
 def water_tray(tray, timestamp, data):
+    '''Creates new WaterEvent for each Plant in specified Tray
+    Requires POST with JSON body containing tray_id and timestamp keys
+    '''
     tray.water_all(timestamp=timestamp)
     return JsonResponse({"action": "water tray", "tray": tray.id}, status=200)
 
@@ -270,6 +297,9 @@ def water_tray(tray, timestamp, data):
 @get_tray_from_post_body
 @get_timestamp_from_post_body
 def fertilize_tray(tray, timestamp, data):
+    '''Creates new FertilizeEvent for each Plant in specified Tray
+    Requires POST with JSON body containing tray_id and timestamp keys
+    '''
     tray.fertilize_all(timestamp=timestamp)
     return JsonResponse({"action": "fertilize tray", "tray": tray.id}, status=200)
 
@@ -278,6 +308,9 @@ def fertilize_tray(tray, timestamp, data):
 @get_plant_from_post_body
 @get_tray_from_post_body
 def add_plant_to_tray(plant, tray, data):
+    '''Adds specified Plant to specified Tray (creates database relation)
+    Requires POST with JSON body containing plant_id and tray_id keys
+    '''
     plant.tray = tray
     plant.save()
     return JsonResponse(
@@ -289,6 +322,9 @@ def add_plant_to_tray(plant, tray, data):
 @requires_json_post
 @get_plant_from_post_body
 def remove_plant_from_tray(plant, data):
+    '''Removes specified Plant from Tray (deletes database relation)
+    Requires POST with JSON body containing plant_id key
+    '''
     plant.tray = None
     plant.save()
     return JsonResponse(
@@ -300,6 +336,9 @@ def remove_plant_from_tray(plant, data):
 @requires_json_post
 @get_tray_from_post_body
 def bulk_add_plants_to_tray(tray, data):
+    '''Adds a list of Plants to specified Tray (creates database relation for each)
+    Requires POST with JSON body containing tray_id and plants (list of UUIDs) keys
+    '''
     added = []
     for plant_id in data["plants"]:
         plant = get_plant_by_uuid(plant_id)
