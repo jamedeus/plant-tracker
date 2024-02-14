7f2a82dd2a27b75afe2546a2c4816ab84c209c57
Josh Medeiros <josh@jamedeus.com>
2024-02-13 20:42:36 -0800

    Add frontend config (react-dom + tailwind + daisyui)
    Webpack writes bundle to backend/plant_tracker/static/plant_tracker where django templates can import

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+function App() {
+    return (
+        <div>
+            <p>Hello World</p>
+        </div>
+    );
+}
+
+export default App;
