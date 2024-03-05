eb98cf6c6972f4cfd7a45d82787ce4b73debe189
Josh Medeiros <josh@jamedeus.com>
2024-03-04 22:16:36 -0800

    Add missing component tests

---
+++ src/components/__tests__/PlantDetails.test.js
@@ -0,0 +1,64 @@
+import { render, within, waitFor } from '@testing-library/react';
+import { DateTime } from 'src/testUtils/luxonMock';
+import PlantDetails from '../PlantDetails';
+import '@testing-library/jest-dom'
+
+describe('App', () => {
+    it('displays correct details', () => {
+        const { getByText, queryByText } = render(
+            <PlantDetails
+                species={"Fittonia"}
+                pot_size={4}
+                description={"Propagated in March 2023"}
+            />
+        );
+        expect(getByText('Fittonia')).toBeInTheDocument();
+        expect(getByText('4')).toBeInTheDocument();
+        expect(getByText('Propagated in March 2023')).toBeInTheDocument();
+        expect(queryByText('No details')).toBeNull();
+    });
+
+    it('hides species row if argument is null', () => {
+        const { getByText, queryByText } = render(
+            <PlantDetails
+                species={null}
+                pot_size={4}
+                description={"Propagated in March 2023"}
+            />
+        );
+        expect(getByText('Species:').parentElement.classList).toContain('hidden');
+        expect(getByText('Description:').parentElement.classList).not.toContain('hidden');
+    });
+
+    it('hides pot_size row if argument is null', () => {
+        const { getByText, queryByText } = render(
+            <PlantDetails
+                species={"Fittonia"}
+                pot_size={null}
+                description={"Propagated in March 2023"}
+            />
+        );
+        expect(getByText('Pot size:').parentElement.classList).toContain('hidden');
+        expect(getByText('Description:').parentElement.classList).not.toContain('hidden');
+    });
+
+    it('hides description row if argument is null', () => {
+        const { getByText, queryByText } = render(
+            <PlantDetails
+                species={"Fittonia"}
+                pot_size={4}
+                description={null}
+            />
+        );
+        expect(getByText('Species:').parentElement.classList).not.toContain('hidden');
+        expect(getByText('Description:').parentElement.classList).toContain('hidden');
+    });
+
+    it('displays "No details" when both arguments are null', () => {
+        const { getByText, queryByText } = render(
+            <PlantDetails species={null} pot_size={null} description={null} />
+        );
+        expect(getByText('No details')).toBeInTheDocument();
+        expect(queryByText('Description:')).toBeNull();
+    });
+});
---
+++ src/components/__tests__/TrayDetails.test.js
@@ -0,0 +1,48 @@
+import { render, within, waitFor } from '@testing-library/react';
+import { DateTime } from 'src/testUtils/luxonMock';
+import TrayDetails from '../TrayDetails';
+import '@testing-library/jest-dom'
+
+describe('App', () => {
+    it('displays correct details', () => {
+        const { getByText, queryByText } = render(
+            <TrayDetails
+                location={"Middle shelf"}
+                description={"Used for propagation"}
+            />
+        );
+        expect(getByText('Middle shelf')).toBeInTheDocument();
+        expect(getByText('Used for propagation')).toBeInTheDocument();
+        expect(queryByText('No details')).toBeNull();
+    });
+
+    it('hides location row if argument is null', () => {
+        const { getByText, queryByText } = render(
+            <TrayDetails
+                location={null}
+                description={"Used for propagation"}
+            />
+        );
+        expect(getByText('Location:').parentElement.classList).toContain('hidden');
+        expect(getByText('Description:').parentElement.classList).not.toContain('hidden');
+    });
+
+    it('hides description row if argument is null', () => {
+        const { getByText, queryByText } = render(
+            <TrayDetails
+                location={"Middle shelf"}
+                description={null}
+            />
+        );
+        expect(getByText('Location:').parentElement.classList).not.toContain('hidden');
+        expect(getByText('Description:').parentElement.classList).toContain('hidden');
+    });
+
+    it('displays "No details" when both arguments are null', () => {
+        const { getByText, queryByText } = render(
+            <TrayDetails location={null} description={null} />
+        );
+        expect(getByText('No details')).toBeInTheDocument();
+        expect(queryByText('Location')).toBeNull();
+    });
+});
