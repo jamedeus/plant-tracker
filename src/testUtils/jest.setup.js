56617d2de7be50058caf37c5146eaa0380d823e6
Josh Medeiros <josh@jamedeus.com>
2024-03-08 22:03:18 -0800

    Fix failing tests
    Issue: scrollIntoView is not implemented in jsdom. This is not an issue on dev machines because the conditional that checks if scrollRef.current is set fails, so scrollIntoView is never called. For some reason on the test runner scrollRef.current is set, so a mock is needed.

--- src/testUtils/jest.setup.js
+++ src/testUtils/jest.setup.js
@@ -4,4 +4,5 @@ beforeAll(() => {
     HTMLDialogElement.prototype.show = jest.fn();
     HTMLDialogElement.prototype.showModal = jest.fn();
     HTMLDialogElement.prototype.close = jest.fn();
+    window.HTMLElement.prototype.scrollIntoView = jest.fn();
 });
