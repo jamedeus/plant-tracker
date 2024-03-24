124ef11691193f3d0f345b1aa198811fdbb2ed1c
Josh Medeiros <josh@jamedeus.com>
2024-03-23 21:40:57 -0700

    Add django admin

--- backend/plant_tracker/admin.py
+++ backend/plant_tracker/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Plant,
+    Tray,
+    Photo,
+    WaterEvent,
+    FertilizeEvent,
+    PruneEvent,
+    RepotEvent,
+)
+
+admin.site.register(Plant)
+admin.site.register(Tray)
+admin.site.register(Photo)
+admin.site.register(WaterEvent)
+admin.site.register(FertilizeEvent)
+admin.site.register(PruneEvent)
+admin.site.register(RepotEvent)
