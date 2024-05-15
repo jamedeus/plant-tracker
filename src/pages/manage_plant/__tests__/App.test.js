1c5a3129045940b78fc1e59df9c6695de3f74477
Josh Medeiros <josh@jamedeus.com>
2024-05-14 20:14:12 -0700

    Fix pipeline

--- src/pages/manage_plant/__tests__/App.test.js
+++ src/pages/manage_plant/__tests__/App.test.js
@@ -12,6 +12,7 @@ describe('App', () => {
     beforeEach(() => {
         // Create mock state objects
         createMockContext('plant', mockContext.plant);
+        createMockContext('notes', mockContext.notes);
         createMockContext('trays', mockContext.trays);
         createMockContext('species_options', mockContext.species_options);
         createMockContext('photo_urls', mockContext.photo_urls);
