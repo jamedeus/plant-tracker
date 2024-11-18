1972d4d7243cb5dde6a9c42bd40c9d95f1248eb2
Josh Medeiros <josh.a.medeiros@gmail.com>
2024-11-18 00:27:07 -0800

    Lint

--- src/pages/archived/App.js
+++ src/pages/archived/App.js
@@ -75,12 +75,6 @@ function App() {
                     <li className="step">Add a sticker to each plant pot</li>
                     <li className="step">Scan codes to register plants!</li>
                 </ul>
-                <button
-                    className="btn btn-accent text-lg"
-                    onClick={() => printModalRef.current.open()}
-                >
-                    Print QR Codes
-                </button>
             </div>
         );
     };
