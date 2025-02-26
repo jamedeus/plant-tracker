54397fd7ef71b44d3ef7dcf070b6c544d62fe64d
Josh Medeiros <josh.a.medeiros@gmail.com>
2025-02-25 21:38:16 -0800

    Fix functional components defined in body
    Avoids a few small rerenders when changing theme (16.2ms before 13.2ms after)

--- src/pages/confirm_new_qr_code/App.js
+++ src/pages/confirm_new_qr_code/App.js
@@ -9,6 +9,47 @@ import { showErrorModal } from 'src/components/ErrorModal';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark, faCheck } from '@fortawesome/free-solid-svg-icons';
 
+// Top left dropdown contents
+const DropdownOptions = () => {
+    // Get toggle theme option from context
+    const { ToggleThemeOption } = useTheme();
+
+    return (
+        <>
+            <li><a onClick={() => window.location.href = "/"}>
+                Overview
+            </a></li>
+            <ToggleThemeOption />
+        </>
+    );
+};
+
+// Navbar title dropdown contents
+const Details = ({ type, instance }) => {
+    switch(type) {
+        case('plant'):
+            return (
+                <PlantDetails
+                    species={instance.species}
+                    pot_size={instance.pot_size}
+                    description={instance.description}
+                />
+            );
+        case('group'):
+            return (
+                <GroupDetails
+                    location={instance.location}
+                    description={instance.description}
+                />
+            );
+    }
+};
+
+Details.propTypes = {
+    type: PropTypes.string.isRequired,
+    instance: PropTypes.object.isRequired
+};
+
 function App() {
     // Load UUIDs and plant/group state from django template context
     const type = parseDomContext("type");
@@ -48,91 +89,6 @@ function App() {
         }
     };
 
-    // Top left dropdown contents
-    const DropdownOptions = () => {
-        // Get toggle theme option from context
-        const { ToggleThemeOption } = useTheme();
-
-        return (
-            <>
-                <li><a onClick={() => window.location.href = "/"}>
-                    Overview
-                </a></li>
-                <ToggleThemeOption />
-            </>
-        );
-    };
-
-    // Navbar title dropdown contents
-    const Details = () => {
-        const Wrapper = ({ children }) => {
-            return (
-                <div className={`card card-compact w-72 p-2 mt-2 mx-auto
-                    shadow bg-neutral text-neutral-content`}>
-                    <div className="card-body">
-                        {children}
-                    </div>
-                </div>
-            );
-        };
-
-        Wrapper.propTypes = {
-            children: PropTypes.node.isRequired
-        };
-
-        switch(type) {
-            case('plant'):
-                return (
-                    <Wrapper>
-                        <PlantDetails
-                            species={instance.species}
-                            pot_size={instance.pot_size}
-                            description={instance.description}
-                        />
-                    </Wrapper>
-                );
-            case('group'):
-                return (
-                    <Wrapper>
-                        <GroupDetails
-                            location={instance.location}
-                            description={instance.description}
-                        />
-                    </Wrapper>
-                );
-        }
-    };
-
-    const Image = () => {
-        return (
-            <div className="mx-auto p-4 bg-base-200 rounded-3xl">
-                <img
-                    className="max-h-[50vh] rounded-xl object-contain"
-                    src={instance.thumbnail}
-                />
-            </div>
-        );
-    };
-
-    const Buttons = () => {
-        return (
-            <div className="flex gap-4 mx-auto mb-8">
-                <button
-                    className="btn btn-error btn-square text-white"
-                    onClick={() => window.location.href = "/"}
-                >
-                    <FontAwesomeIcon className="h-6 w-6" icon={faXmark} />
-                </button>
-                <button
-                    className="btn btn-success btn-square text-white"
-                    onClick={handleConfirm}
-                >
-                    <FontAwesomeIcon className="h-6 w-6" icon={faCheck} />
-                </button>
-            </div>
-        );
-    };
-
     return (
         <div className="container flex flex-col mx-auto h-screen">
             <Navbar
@@ -141,7 +97,12 @@ function App() {
                 }
                 title={instance.display_name}
                 titleOptions={
-                    <Details />
+                    <div className={`card card-compact w-72 p-2 mt-2 mx-auto
+                        shadow bg-neutral text-neutral-content`}>
+                        <div className="card-body">
+                            <Details type={type} instance={instance} />
+                        </div>
+                    </div>
                 }
             />
 
@@ -149,8 +110,30 @@ function App() {
                 <p className="text-lg font-bold">
                     Is this the new QR code for your {type}?
                 </p>
-                {instance.thumbnail && <Image />}
-                <Buttons />
+                {instance.thumbnail && (
+                    <div className="mx-auto p-4 bg-base-200 rounded-3xl">
+                        <img
+                            className="max-h-[50vh] rounded-xl object-contain"
+                            src={instance.thumbnail}
+                        />
+                    </div>
+                )}
+
+                {/* Confirm/cancel buttons */}
+                <div className="flex gap-4 mx-auto mb-8">
+                    <button
+                        className="btn btn-error btn-square text-white"
+                        onClick={() => window.location.href = "/"}
+                    >
+                        <FontAwesomeIcon className="h-6 w-6" icon={faXmark} />
+                    </button>
+                    <button
+                        className="btn btn-success btn-square text-white"
+                        onClick={handleConfirm}
+                    >
+                        <FontAwesomeIcon className="h-6 w-6" icon={faCheck} />
+                    </button>
+                </div>
             </div>
         </div>
     );
