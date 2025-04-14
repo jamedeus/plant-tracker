ba52fd9e5470cd03a776d878bf3b7bf0c2b714a8
Josh Medeiros <josh.a.medeiros@gmail.com>
2025-04-14 15:11:31 -0700

    Fix database access during app initialization warning
    Don't need to create DEFAULT_USER, already created in migration 0018_create_default_user

--- backend/plant_tracker/apps.py
+++ backend/plant_tracker/apps.py
@@ -1,8 +1,6 @@
 import os
 import sys
-from django.conf import settings
 from django.apps import AppConfig
-from django.contrib.auth import get_user_model
 
 
 class PlantTrackerConfig(AppConfig):
@@ -10,19 +8,9 @@ class PlantTrackerConfig(AppConfig):
     name = "plant_tracker"
 
     def ready(self):
-        '''Ensure default user exists (if SINGLE_USER_MODE enabled) and rebuild
-        all cached state objects when the server starts (prevents serving
-        outdated cached state if database was modified offline).
+        '''Rebuild all cached state objects when the server starts.
+        Prevents serving outdated cached state if database was modified offline.
         '''
-
-        if settings.SINGLE_USER_MODE:
-            User = get_user_model()
-            if not User.objects.filter(username=settings.DEFAULT_USERNAME).exists():
-                User.objects.create_user(
-                    username=settings.DEFAULT_USERNAME,
-                    password='root'
-                )
-
         if 'test' in sys.argv or 'pylint' in str(sys.argv):
             # Skip cache updates when running unit tests or pylint
             return
