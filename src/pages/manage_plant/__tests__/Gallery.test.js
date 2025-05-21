cdd8d67f9fab08d739eeaaa5d5738eae91424e75
Josh Medeiros <josh.a.medeiros@gmail.com>
2025-05-20 17:31:55 -0700

    Move Gallery tests to new suite
    Reduces test run time by 5 seconds, fixes slowdown noted in 60a20c92. Seems like long test suites don't scale well, could probably speed up further by splitting.

---
+++ src/pages/manage_plant/__tests__/Gallery.test.js
@@ -0,0 +1,115 @@
+import createMockContext from 'src/testUtils/createMockContext';
+import bulkCreateMockContext from 'src/testUtils/bulkCreateMockContext';
+import App from '../App';
+import { PageWrapper } from 'src/index';
+import { mockContext } from './mockContext';
+
+describe('Gallery', () => {
+    let app, user;
+
+    beforeAll(() => {
+        // Create mock state objects
+        bulkCreateMockContext(mockContext);
+        createMockContext('user_accounts_enabled', true);
+    });
+
+    beforeEach(() => {
+        // Allow fast forwarding (must hold delete note button to confirm)
+        jest.useFakeTimers({ doNotFake: ['Date'] });
+
+        // Render app + create userEvent instance to use in tests
+        user = userEvent.setup({ advanceTimers: jest.advanceTimersByTimeAsync });
+        app = render(
+            <PageWrapper>
+                <App />
+            </PageWrapper>
+        );
+    });
+
+    // Clean up pending timers after each test
+    afterEach(() => {
+        jest.runAllTimers();
+        jest.useRealTimers();
+    });
+
+    it('opens photo gallery when dropdown option is clicked', async () => {
+        // Confirm gallery div does not exist
+        expect(document.body.querySelector('.yarl__root')).toBeNull();
+
+        // Click image thumbnail, confirm gallery appears
+        await user.click(app.getByRole('button', {name: 'Gallery'}));
+        expect(document.body.querySelector('.yarl__root')).not.toBeNull();
+
+        // Click close button, confirm gallery disappears
+        await user.click(app.getByRole('button', {name: 'Close'}));
+        await jest.advanceTimersByTimeAsync(500);
+        expect(document.body.querySelector('.yarl__root')).toBeNull();
+    });
+
+    it('opens photo gallery when timeline photo thumbnails are clicked', async () => {
+        // Confirm gallery div does not exist
+        expect(document.body.querySelector('.yarl__root')).toBeNull();
+
+        // Click first timeline image thumbnail, confirm gallery appears
+        const photoThumbnail = document.body.querySelector('img.photo-thumbnail-timeline');
+        await user.click(photoThumbnail);
+        expect(document.body.querySelector('.yarl__root')).not.toBeNull();
+        // Confirm visible slide src is full-res version of clicked thumbnail
+        expect(document.querySelector('.yarl__slide_current img').src).toBe(
+            photoThumbnail.src
+                .replace('/media/thumbnails', '/media/images')
+                .replace('_thumb', '')
+        );
+    });
+
+    it('remembers the current gallery photo and reopens next time gallery is opened', async () => {
+        // Open photo gallery, confirm most-recent photo is visible
+        await user.click(app.getByRole('button', {name: 'Gallery'}));
+        expect(document.querySelector('.yarl__slide_current img').src).toEndWith(
+            '/media/images/photo3.jpg'
+        );
+
+        // Click next photo button, confirm second most-recent photo is visible
+        await user.click(app.getByRole('button', {name: 'Next'}));
+        expect(document.querySelector('.yarl__slide_current img').src).toEndWith(
+            '/media/images/photo2.jpg'
+        );
+
+        // Close gallery, confirm closed
+        await user.click(app.getByRole('button', {name: 'Close'}));
+        await jest.advanceTimersByTimeAsync(500);
+        expect(document.body.querySelector('.yarl__root')).toBeNull();
+
+        // Reopen gallery, confirm last-viewed photo is visible (not default)
+        await user.click(app.getByRole('button', {name: 'Gallery'}));
+        expect(document.querySelector('.yarl__slide_current img').src).toEndWith(
+            '/media/images/photo2.jpg'
+        );
+    });
+
+    it('shows progress bar while gallery slideshow is playing', async () => {
+        // Open photo gallery, confirm progress bar is not rendered
+        await user.click(app.getByRole('button', {name: 'Gallery'}));
+        expect(document.querySelector('.slideshow_progress_bar')).toBeNull();
+
+        // Start slideshow, confirm progress bar appeared
+        await user.click(app.getByRole('button', {name: 'Play'}));
+        expect(document.querySelector('.slideshow_progress_bar')).not.toBeNull();
+
+        // Confirm visible slide changes every 3000ms (had issues in development
+        // where if photoGalleryIndexChanged was not called for each slide the
+        // progress bar render would cause slideshow to go back to first slide)
+        expect(document.querySelector('.yarl__slide_current img').src).toEndWith(
+            '/media/images/photo3.jpg'
+        );
+        await jest.advanceTimersByTimeAsync(3000);
+        expect(document.querySelector('.yarl__slide_current img').src).toEndWith(
+            '/media/images/photo2.jpg'
+        );
+
+        // Confirm progress bar disappears when slideshow stopped
+        expect(document.querySelector('.slideshow_progress_bar')).not.toBeNull();
+        await user.click(app.getByRole('button', {name: 'Pause'}));
+        expect(document.querySelector('.slideshow_progress_bar')).toBeNull();
+    });
+});
