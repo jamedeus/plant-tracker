jamedeus/plant-tracker

commit 803b54b6c239dbce2e537cc0a14db6215f47731b

Improve accessibility

src/pages/manage_plant/Gallery.js
@@ -150,6 +150,13 @@ const Gallery = () => {
                     }
                 }
             }}
+            labels={{
+                Next: "Next photo",
+                Prev: "Previous photo",
+                Close: "Close photo gallery",
+                Play: "Play photo slideshow",
+                Pause: "Pause photo slideshow"
+            }}
             controller={{
                 closeOnPullDown: true,
                 closeOnBackdropClick: true

src/pages/manage_plant/__tests__/__snapshots__/Gallery.snapshot.test.js.snap
@@ -330,9 +330,9 @@ exports[`Gallery matches snapshot for desktop layout 1`] = `
                   </svg>
                 </button>
                 <button
-                  aria-label="Play"
+                  aria-label="Play photo slideshow"
                   class="yarl__button"
-                  title="Play"
+                  title="Play photo slideshow"
                   type="button"
                 >
                   <svg
@@ -372,9 +372,9 @@ exports[`Gallery matches snapshot for desktop layout 1`] = `
                   </svg>
                 </button>
                 <button
-                  aria-label="Close"
+                  aria-label="Close photo gallery"
                   class="yarl__button"
-                  title="Close"
+                  title="Close photo gallery"
                   type="button"
                 >
                   <svg
@@ -415,9 +415,9 @@ exports[`Gallery matches snapshot for desktop layout 1`] = `
                 </svg>
               </button>
               <button
-                aria-label="Next"
+                aria-label="Next photo"
                 class="yarl__button yarl__navigation_next"
-                title="Next"
+                title="Next photo"
                 type="button"
               >
                 <svg
@@ -911,9 +911,9 @@ exports[`Gallery matches snapshot for desktop layout 2`] = `
                   </svg>
                 </button>
                 <button
-                  aria-label="Play"
+                  aria-label="Play photo slideshow"
                   class="yarl__button"
-                  title="Play"
+                  title="Play photo slideshow"
                   type="button"
                 >
                   <svg
@@ -953,9 +953,9 @@ exports[`Gallery matches snapshot for desktop layout 2`] = `
                   </svg>
                 </button>
                 <button
-                  aria-label="Close"
+                  aria-label="Close photo gallery"
                   class="yarl__button"
-                  title="Close"
+                  title="Close photo gallery"
                   type="button"
                 >
                   <svg
@@ -996,9 +996,9 @@ exports[`Gallery matches snapshot for desktop layout 2`] = `
                 </svg>
               </button>
               <button
-                aria-label="Next"
+                aria-label="Next photo"
                 class="yarl__button yarl__navigation_next"
-                title="Next"
+                title="Next photo"
                 type="button"
               >
                 <svg

src/pages/manage_plant/Settings.js
@@ -259,7 +259,7 @@ const Settings = () => {
                     <button
                         className="btn btn-ghost btn-circle size-12"
                         onClick={() => dispatch(settingsMenuOpened(false))}
-                        data-testid="settings-menu-close-button"
+                        aria-label="Close settings menu"
                     >
                         <CloseButtonIcon />
                     </button>

src/pages/manage_plant/__tests__/Gallery.test.js
@@ -43,7 +43,7 @@ describe('Gallery', () => {
         expect(document.body.querySelector('.yarl__root')).not.toBeNull();
 
         // Click close button, confirm gallery disappears
-        await user.click(app.getByRole('button', {name: 'Close'}));
+        await user.click(app.getByRole('button', {name: 'Close photo gallery'}));
         await jest.advanceTimersByTimeAsync(500);
         expect(document.body.querySelector('.yarl__root')).toBeNull();
     });
@@ -71,14 +71,14 @@ describe('Gallery', () => {
             '/media/images/photo3.jpg'
         );
 
-        // Click next photo button, confirm second most-recent photo is visible
-        await user.click(app.getByRole('button', {name: 'Next'}));
+        // Click Next photo button, confirm second most-recent photo is visible
+        await user.click(app.getByRole('button', {name: 'Next photo'}));
         expect(document.querySelector('.yarl__slide_current img').src).toEndWith(
             '/media/images/photo2.jpg'
         );
 
         // Close gallery, confirm closed
-        await user.click(app.getByRole('button', {name: 'Close'}));
+        await user.click(app.getByRole('button', {name: 'Close photo gallery'}));
         await jest.advanceTimersByTimeAsync(500);
         expect(document.body.querySelector('.yarl__root')).toBeNull();
 
@@ -95,7 +95,7 @@ describe('Gallery', () => {
         expect(document.querySelector('.slideshow_progress_bar')).toBeNull();
 
         // Start slideshow, confirm progress bar appeared
-        await user.click(app.getByRole('button', {name: 'Play'}));
+        await user.click(app.getByRole('button', {name: 'Play photo slideshow'}));
         expect(document.querySelector('.slideshow_progress_bar')).not.toBeNull();
 
         // Confirm visible slide changes every 3000ms (had issues in development
@@ -111,7 +111,7 @@ describe('Gallery', () => {
 
         // Confirm progress bar disappears when slideshow stopped
         expect(document.querySelector('.slideshow_progress_bar')).not.toBeNull();
-        await user.click(app.getByRole('button', {name: 'Pause'}));
+        await user.click(app.getByRole('button', {name: 'Pause photo slideshow'}));
         expect(document.querySelector('.slideshow_progress_bar')).toBeNull();
     });
 
@@ -144,8 +144,8 @@ describe('Gallery', () => {
         expect(window.HTMLElement.prototype.scrollIntoView).not.toHaveBeenCalled();
 
         // Change slide, close gallery, confirm scrollIntoView was called
-        await user.click(app.getByRole('button', {name: 'Next'}));
-        await user.click(app.getByRole('button', {name: 'Close'}));
+        await user.click(app.getByRole('button', {name: 'Next photo'}));
+        await user.click(app.getByRole('button', {name: 'Close photo gallery'}));
         await jest.advanceTimersByTimeAsync(500);
         expect(window.HTMLElement.prototype.scrollIntoView).toHaveBeenCalled();
     });
@@ -156,7 +156,7 @@ describe('Gallery', () => {
         expect(window.HTMLElement.prototype.scrollIntoView).not.toHaveBeenCalled();
 
         // Close gallery without changing slides, confirm scrollIntoView was NOT called
-        await user.click(app.getByRole('button', {name: 'Close'}));
+        await user.click(app.getByRole('button', {name: 'Close photo gallery'}));
         await jest.advanceTimersByTimeAsync(500);
         expect(window.HTMLElement.prototype.scrollIntoView).not.toHaveBeenCalled();
     });
@@ -173,8 +173,8 @@ describe('Gallery', () => {
         expect(window.HTMLElement.prototype.scrollIntoView).not.toHaveBeenCalled();
 
         // Change slide, close gallery
-        await user.click(app.getByRole('button', {name: 'Next'}));
-        await user.click(app.getByRole('button', {name: 'Close'}));
+        await user.click(app.getByRole('button', {name: 'Next photo'}));
+        await user.click(app.getByRole('button', {name: 'Close photo gallery'}));
         await jest.advanceTimersByTimeAsync(500);
         // Confirm scrollIntoView was NOT called (photo already visible)
         expect(window.HTMLElement.prototype.scrollIntoView).not.toHaveBeenCalled();

src/pages/manage_plant/__tests__/Settings.test.js
@@ -45,7 +45,7 @@ describe('Settings menu', () => {
         // Open again, click close button, confirm closed
         await user.click(app.getByTestId('open-settings-menu'));
         expect(app.container.querySelector('dialog.settings-menu').open).toBeTrue();
-        await user.click(app.getByTestId('settings-menu-close-button'));
+        await user.click(app.getByLabelText('Close settings menu'));
         expect(app.container.querySelector('dialog.settings-menu').open).toBeFalse();
     });
 

src/pages/manage_plant/__tests__/__snapshots__/App.snapshot.test.js.snap
@@ -1691,8 +1691,8 @@ exports[`App matches snapshot 1`] = `
               Settings
             </h2>
             <button
+              aria-label="Close settings menu"
               class="btn btn-ghost btn-circle size-12"
-              data-testid="settings-menu-close-button"
             >
               <svg
                 aria-hidden="true"
@@ -4050,8 +4050,8 @@ exports[`App matches snapshot 1`] = `
             Settings
           </h2>
           <button
+            aria-label="Close settings menu"
             class="btn btn-ghost btn-circle size-12"
-            data-testid="settings-menu-close-button"
           >
             <svg
               aria-hidden="true"
@@ -6372,8 +6372,8 @@ exports[`App matches snapshot when plant is archived 1`] = `
               Settings
             </h2>
             <button
+              aria-label="Close settings menu"
               class="btn btn-ghost btn-circle size-12"
-              data-testid="settings-menu-close-button"
             >
               <svg
                 aria-hidden="true"
@@ -8597,8 +8597,8 @@ exports[`App matches snapshot when plant is archived 1`] = `
             Settings
           </h2>
           <button
+            aria-label="Close settings menu"
             class="btn btn-ghost btn-circle size-12"
-            data-testid="settings-menu-close-button"
           >
             <svg
               aria-hidden="true"

commit 708f7e7a319027765511975e9704c22a7b7cf3b4

Fix gallery button regression
Custom aria labels added in 803b54b6 broke CSS added in 74057020 that increased prev/next button clickable area

src/css/gallery.css
@@ -90,12 +90,12 @@ button.yarl__thumbnails_thumbnail {
 }
 
 /* Next/prev buttons fill height (click anywhere along edge of screen) */
-.yarl__button[title='Next'],
-.yarl__button[title='Prev'] {
+.yarl__button[title='Next photo'],
+.yarl__button[title='Previous photo'] {
     @apply h-full;
 }
 
 /* Prevent next button covering close button */
-.yarl__button[title='Close'] {
+.yarl__button[title='Close photo gallery'] {
     @apply z-100;
 }

src/pages/manage_plant/Gallery.js
@@ -165,7 +165,7 @@ const Gallery = () => {
             }}
             labels={{
                 Next: "Next photo",
-                Prev: "Previous photo",
+                Previous: "Previous photo",
                 Close: "Close photo gallery",
                 Play: "Play photo slideshow",
                 Pause: "Pause photo slideshow",

src/pages/manage_plant/__tests__/__snapshots__/Gallery.snapshot.test.js.snap
@@ -415,9 +415,9 @@ exports[`Gallery matches snapshot for desktop layout 1`] = `
                 </button>
               </div>
               <button
-                aria-label="Previous"
+                aria-label="Previous photo"
                 class="yarl__button yarl__navigation_prev"
-                title="Previous"
+                title="Previous photo"
                 type="button"
               >
                 <svg
@@ -1017,9 +1017,9 @@ exports[`Gallery matches snapshot for desktop layout 2`] = `
                 </button>
               </div>
               <button
-                aria-label="Previous"
+                aria-label="Previous photo"
                 class="yarl__button yarl__navigation_prev"
-                title="Previous"
+                title="Previous photo"
                 type="button"
               >
                 <svg