ee3d839164ede866b28a04a73254ad5f0756e7e9
Josh Medeiros <josh.a.medeiros@gmail.com>
2025-07-26 17:31:37 -0700

    Sphinx read the docs theme
    Way better, should be default

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -30,11 +30,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '*migrations*', '*tests*
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
-
-# Add custom CSS
-html_css_files = getattr(globals(), 'html_css_files', [])
+html_theme = "sphinx_rtd_theme"
 
 sys.path.insert(0, os.path.abspath(os.path.join('..', '..', 'backend')))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
