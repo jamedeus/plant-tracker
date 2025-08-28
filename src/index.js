c40a322cb25d19483becb83577f917073aa96900
Josh Medeiros <josh.a.medeiros@gmail.com>
2025-08-27 20:58:21 -0700

    Simplify
    No reason to have a function here (can't import it since it's called immediately)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import { createRoot } from 'react-dom/client';
-import router from 'src/routes';
 import AppRoot from './AppRoot';
+import router from './routes';
 import 'src/css/index.css';
 
-/* istanbul ignore next */
-function bootstrapSpa() {
-    const container = document.getElementById('root');
-    const root = createRoot(container);
-    root.render(<AppRoot router={router} />);
-}
-
-bootstrapSpa();
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<AppRoot router={router} />);
