0b783aff410240558cd472d81016de902780244f
Josh Medeiros <josh.a.medeiros@gmail.com>
2025-08-31 14:51:13 -0700

    Fix cache not rebuilding when server restarts
    Issue: the RUN_MAIN env var is only set by the django dev server, so once the docker image switched to gunicorn this was skipped every time. Once that was fixed it ran once per gunicorn worker (fixed with redis lock key to prevent running more than once every 30 seconds).

--- backend/plant_tracker/apps.py
+++ backend/plant_tracker/apps.py
@@ -2,6 +2,7 @@ import os
 import sys
 from django.conf import settings
 from django.apps import AppConfig
+from django.core.cache import caches
 from django.utils.autoreload import autoreload_started
 
 
@@ -16,10 +17,18 @@ class PlantTrackerConfig(AppConfig):
         if 'test' in sys.argv or 'pylint' in str(sys.argv):
             # Skip cache updates when running unit tests or pylint
             return
-        if not os.environ.get('RUN_MAIN'):
-            # Prevent running when celery starts, running twice when dev server
-            # starts (dev server starts second process to detect file changes)
+
+        if 'manage.py' in sys.argv[0] and 'runserver' not in sys.argv:
+            # Skip cache updates when running management commands
             return
 
-        from .tasks import update_all_cached_states
-        update_all_cached_states.delay()
+        if 'celery' in sys.argv[0]:
+            # Skip cache updates when starting celery worker
+            return
+
+        # Prevent cache update running for each gunicorn worker
+        # Sets cache key that expires in 30 seconds, only runs if not set
+        if not caches["default"].get('startup_update_cached_states'):
+            caches["default"].set('startup_update_cached_states', 1, 30)
+            from .tasks import update_all_cached_states
+            update_all_cached_states.delay()
