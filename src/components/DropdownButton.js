2c70f5489b73c18c7954ff4f7eb35120357e0e69
Josh Medeiros <josh.a.medeiros@gmail.com>
2025-09-08 17:17:10 -0700

    Close dropdown on second click (CSS only)
    Mostly reverts ccf3d992 but keeping the component to reduce boilerplate, ensure titles + aria labels, etc.
    
    Dropdown CSS was ejected from daisyui v4 which had this limitation (can't click again to close when using focus-based dropdown). This was fixed in daisyui v5 using the the pointer-events-none trick to remove focus on second click (see https://github.com/saadeghi/daisyui/blob/4f3c74540351808b884759cb0c4d8e968867ea18/packages/daisyui/src/components/dropdown.css#L37).

--- src/components/DropdownButton.js
+++ src/components/DropdownButton.js
@@ -2,14 +2,6 @@ import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 
 const DropdownButton = memo(function DropdownButton({ className, title, onClick, children }) {
-    // Close dropdown if button clicked again while open (remove focus)
-    const handlePointerDown = (e) => {
-        if (document.activeElement === e.currentTarget) {
-            e.preventDefault();
-            document.activeElement.blur();
-        }
-    };
-
     return (
         <div
             tabIndex={0}
@@ -18,7 +10,6 @@ const DropdownButton = memo(function DropdownButton({ className, title, onClick,
             title={title}
             aria-label={title}
             onClick={onClick}
-            onPointerDown={handlePointerDown}
         >
             {children}
         </div>
